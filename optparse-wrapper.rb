44ec89375ff6710d1fab33bce2c820e7ad2e25ca
Florian Pilz <fpilz87@googlemail.com>
2011-02-20 16:54:03 +0100

    support options with arguments

--- optparse-wrapper.rb
+++ optparse-wrapper.rb
@@ -5,21 +5,41 @@ class Parser
   def initialize
     @options = []
     yield self
-    process!
   end
   
   def banner(text)
     @banner = text
   end
   
+  def option(name, desc, setting)
+    option = OpenStruct.new
+    option.name = name
+    option.desc = desc
+    option.default = setting[:default]
+    @options << option
+  end
+  
   def process!
+    opts = {}
     optparser = OptionParser.new do |p|
       p.banner = @banner
+      @options.each do |o|
+        opts[o.name] = o.default
+        klass = o.default.class
+        klass = Integer if klass == Fixnum
+        p.on("-" << o.name.to_s.chars.first, "--" << o.name.to_s << " " << o.default, klass, o.desc) do |x|
+          opts[o.name] = x
+        end
+      end
     end
-    optparser.parse!
+    optparser.parse!(ARGV)
+    opts
   end
 end
 
 options = Parser.new do |p|
   p.banner "test"
-end
+  p.option :verbose, "set verbosity", :default => 4
+end.process!
+
+puts options[:verbose]

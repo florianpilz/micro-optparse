dd9501b2dc4feb4f998b7b143dae31e8c5cbc0be
Florian Pilz <fpilz87@googlemail.com>
2011-02-26 15:39:07 +0100

    comment out test-code

--- lib/micro-optparse.rb
+++ lib/micro-optparse.rb
@@ -70,21 +70,21 @@ module Micro
     end
   end
 end
-
-options = Micro::Optparse.new do |p|
-  # p.banner = "test"
-  p.version = "OptParseWrapper 0.8 (c) Florian Pilz 2011"
-  p.option :severity, "set severity", :default => 4, :value_in_set => [4,5,6,7,8]
-  p.option :verbose, "enable verbose output"
-  p.option :mutation, "set mutation", :default => "MightyMutation", :value_matches => /Mutation/
-  p.option :plus_selection, "use plus-selection if set", :default => true
-  p.option :selection, "selection used", :default => "BestSelection"#, :short => "l"
-  p.option :chance, "set mutation chance", :default => 0.8, :value_satisfies => lambda {|x| x >= 0.0 && x <= 1.0}
-end.process!
-
-puts options[:severity]
-puts options[:verbose]
-puts options[:mutation]
-puts options[:plus_selection]
-puts options[:selection]
-puts options[:chance]
+# 
+# options = Micro::Optparse.new do |p|
+#   # p.banner = "test"
+#   p.version = "OptParseWrapper 0.8 (c) Florian Pilz 2011"
+#   p.option :severity, "set severity", :default => 4, :value_in_set => [4,5,6,7,8]
+#   p.option :verbose, "enable verbose output"
+#   p.option :mutation, "set mutation", :default => "MightyMutation", :value_matches => /Mutation/
+#   p.option :plus_selection, "use plus-selection if set", :default => true
+#   p.option :selection, "selection used", :default => "BestSelection"#, :short => "l"
+#   p.option :chance, "set mutation chance", :default => 0.8, :value_satisfies => lambda {|x| x >= 0.0 && x <= 1.0}
+# end.process!
+# 
+# puts options[:severity]
+# puts options[:verbose]
+# puts options[:mutation]
+# puts options[:plus_selection]
+# puts options[:selection]
+# puts options[:chance]

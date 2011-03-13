2614714eb31d3705429dd454f81719a4f8e24839
Florian Pilz <fpilz87@googlemail.com>
2011-03-13 18:31:30 +0100

    add missing tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -15,11 +15,31 @@ describe Parser do
       result.should include("This is a banner")
     end
     
+    it "should include the default banner info if no banner message was set" do
+      result = `ruby spec/programs/empty_example.rb --help`
+      result.should include("Usage: empty_example [options]")
+    end
+    
     it "should display the version when called with --version or -V" do
       # here -V is used for version, as -v is already taken for the verbose switch
       results = [`ruby spec/programs/example.rb -V`, `ruby spec/programs/example.rb --version`]
       results.each do |result|
         result.strip.should == "OptParseWrapper 0.8 (c) Florian Pilz 2011"
+      end
+    end
+    
+    it "should display the version when called with -v" do
+      result = `ruby spec/programs/another_example.rb -v`
+      result.strip.should == "OptParseWrapper 0.8 (c) Florian Pilz 2011"
+    end
+    
+    it "should display a warning when --version or -v was called but no version was set" do
+      results = [
+        `ruby spec/programs/empty_example.rb --version 2>&1`,
+        `ruby spec/programs/empty_example.rb -v 2>&1`
+      ]
+      results.each do |result|
+        result.strip.should == "empty_example: version unknown"
       end
     end
     
@@ -31,6 +51,11 @@ describe Parser do
       result.should include(":plus_selection => true")
       result.should include(":selection => BestSelection")
       result.should include(":chance => 0.8")
+    end
+    
+    it "should assume false as default value if no default value was given" do
+      result = `ruby spec/programs/another_example.rb`
+      result.should include(":eat_nothing => false")
     end
     
     it "should display overwritten values accordingly when long option names were used" do
@@ -90,6 +115,24 @@ describe Parser do
     it "should display a warning if validation value_satisfies failed" do
       result = `ruby spec/programs/example.rb --chance 300.21`
       result.strip.should == "Parameter must satisfy given conditions (see description)"
+    end
+    
+    it "should validate all given validations" do
+      result = `ruby spec/programs/another_example.rb --eat-cake VanillaBrownie`
+      result.strip.should == "Parameter must match /Cake/"
+      
+      result = `ruby spec/programs/another_example.rb --eat-cake 2VanillaCakes`
+      result.strip.should == "Parameter must satisfy given conditions (see description)"
+    end
+    
+    it "should assign a different character for the short accessor if the first / second / ... is already taken" do
+      result = `ruby spec/programs/another_example.rb --help`
+      result.should include("-e, --eat-cake")
+      result.should include("-a, --eat-salad")
+      result.should include("-t, --eat-bagel")
+      result.should include("-n, --[no]-eat-nothing")
+      result.should include("-m, --eat-marshmellow")
+      result.should include("--eat-me")
     end
   end
 end
--- spec/programs/another_example.rb
+++ spec/programs/another_example.rb
@@ -3,9 +3,12 @@ require "micro-optparse"
 
 options = Parser.new do |p|
   p.version = "OptParseWrapper 0.8 (c) Florian Pilz 2011"
-  p.option :eat_cake, "Eath the yummy cake!", :default => "PlainCake", :value_in_set => ["PlainCake", "VanillaCake"], :value_matches => /Cake/, :value_satisfies => lambda { |arg| arg.to_i == 0 }
+  p.option :eat_cake, "Eath the yummy cake!", :default => "PlainCake", :value_matches => /Cake/, :value_satisfies => lambda { |arg| arg.to_i == 0 }
   p.option :eat_salad, "It's healty!", :default => "CucumberSalad"
   p.option :eat_bagel, "You should try it with salmon.", :default => "SalmonBagel"
+  p.option :eat_nothing, "Stupid decision ..."
+  p.option :eat_marshmellow, "Filled with sugar.", :default => "Sugar"
+  p.option :eat_me, "WHAT?!?", :default => "TastyHuman"
 end.process!
 
 options.each_pair do |key, value|
